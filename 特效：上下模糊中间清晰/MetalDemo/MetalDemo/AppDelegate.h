cdeafb955ecbfe86e3c247b8a4d88d355368cf8f
Piansu <suruochang@gmail.com>
2018-10-22 21:11:42 +0800

    导入第三个特效

---
+++ 特效：上下模糊中间清晰/MetalDemo/MetalDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MetalDemo
+//
+//  Created by suruochang on 2018/10/22.
+//  Copyright © 2018年 suruochang. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
