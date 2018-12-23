0a3077070f17c5b8beb1262f4c40d3d0dd44e348
Piansu <suruochang@gmail.com>
2018-12-23 14:10:22 +0800

    add files

---
+++ YUVPlayer_OpenGL/YUVPlayer/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  YUVPlayer
+//
+//  Created by suruochang on 2019/1/1.
+//  Copyright © 2019年 Su Ruochang. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
